1cb9d0867e26529e9f284711484f0ca9a35cc75d
Miguel Rengifo <miguelreng@gmail.com>
2024-03-14 11:21:16 +0800

    codebase

---
+++ framerusercontent.com/sites/1jVm6SKTPYxdcaJiguXAwZ/chunk-K72JTZR4.mjs
@@ -0,0 +1,11 @@
+var e=t=>typeof t=="object"&&t!==null&&typeof t.src=="string"?t.src:typeof t=="string"?t:void 0,n=t=>typeof t=="string"?t:String(t),r=t=>({bodyClassName:"framer-body-jLwmxjUuM",breakpoints:[{hash:"x1sdrq",mediaQuery:"(min-width: 1900px)"},{hash:"eaagav",mediaQuery:"(min-width: 1200px) and (max-width: 1899px)"},{hash:"yx6nw3",mediaQuery:"(max-width: 1199px)"}],customHTMLHeadStart:`<style>
+    img.framer-text.framer-image {
+        border-radius: 1em !important;
+        margin: 0 auto;
+        width: 100% !important;
+    }
+
+    body {
+        background: #f5f6fa !important;
+    }</head></style>`,elements:{QqIDnTS5lfdqlhUtsY:"clients",QqIDnTS5lSxLXR8dP0:"about",QqIDnTS5lUOUeBY1Tc:"work"},framerSearch:{index:!0},robots:"noindex",socialImage:e(t?.DV6R5W2Pr),title:`${t?.OTDcDAIip!==void 0?n(t.OTDcDAIip):"{{OTDcDAIip}}"}`,viewport:"width=device-width"}),d=r,s=1,m={exports:{default:{type:"variable",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{d as a,s as b,m as c};
+//# sourceMappingURL=chunk-K72JTZR4.mjs.map
