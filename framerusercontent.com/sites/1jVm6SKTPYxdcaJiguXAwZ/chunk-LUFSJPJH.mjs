4507f6597350c25046f503d8ab99dd6cf9e08d86
Miguel Rengifo <miguelreng@gmail.com>
2025-09-03 17:18:13 -0500

    Website updated

--- framerusercontent.com/sites/1jVm6SKTPYxdcaJiguXAwZ/chunk-LUFSJPJH.mjs
+++
@@ -1,7 +0,0 @@
-function e(t,a){return{bodyClassName:"framer-body-q6_QsX_WL",breakpoints:[{hash:"fwleab",mediaQuery:"(min-width: 1900px)"},{hash:"i2guz9",mediaQuery:"(min-width: 1200px) and (max-width: 1899px)"},{hash:"q6yh30",mediaQuery:"(min-width: 810px) and (max-width: 1199px)"},{hash:"hwcb6t",mediaQuery:"(max-width: 809px)"}],customHTMLHeadStart:`<style>
-body {
-    background: #f5f6fa !important;
-}
-</style>
-`,description:"We believe the human element within the digital world is essential for creating an impactful design and user experience, we create innovative digital products that will embrace and define your brand.",elements:{fdqlhUtsY:"clients",jbOSzrS02:"testimonies-1",OFZfq_yiY:"testimonies",SxLXR8dP0:"about",UOUeBY1Tc:"work",Vqxvpedkx:"testimonies-2"},framerSearch:{index:!0},socialImage:"https://framerusercontent.com/images/mX7FEloAO6Od2oFGvmzQweWAU.png?scale-down-to=4096",title:"96 - A design studio for founders",viewport:"width=device-width"}}var o=1,d={exports:{default:{type:"function",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{e as a,o as b,d as c};
-//# sourceMappingURL=chunk-LUFSJPJH.mjs.map
